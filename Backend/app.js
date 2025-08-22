2f086c275cb60edbf5a23329a823cfddfe24ca0a
Masuk Khan <masukkhanemail@gmail.com>
2025-08-23 00:45:05 +0530

    changes

--- Backend/app.js
+++ Backend/app.js
@@ -1,45 +1,43 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
 
-import {PORT} from './config/env.js';
- 
+import { PORT } from './config/env.js';
 import userRouter from './routes/user.routes.js';
-
 import connectToDatabase from './database/mongoose.js';
 
 
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 
 app.set('trust proxy', true);
 
 app.use(cors({
-  origin: "*",    
+  origin: "*",
   credentials: true
 }));
 
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 
-
+app.use(express.static(path.join(__dirname, '../Frontend/signup.html')));
 
 app.use('/', userRouter);
 
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../Frontend/signup.html', 'signup.html'));
+});
 
-
-app.get("/", (req,res)=>{
-    res.send('Welcome to the user management API!');
-})
-
-app.listen(PORT,async ()=>{
-    console.log(`user management api is running on http://localhost:${PORT}`);
-
-   await connectToDatabase();
-})
+app.listen(PORT, async () => {
+  console.log(`user management api is running on http://localhost:${PORT}`);
+  await connectToDatabase();
+});
 
 export default app;
